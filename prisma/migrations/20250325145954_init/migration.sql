22cca9a84e4e0a4145996fdf67f2c0578df80913
Sabbalingurinn <s.b.snorrason@gmail.com>
2025-03-25 15:00:49 +0000

    loka skref

--- prisma/migrations/20250325145954_init/migration.sql
+++ prisma/migrations/20250325145954_init/migration.sql
@@ -1,4 +1,13 @@
 -- CreateTable
+CREATE TABLE "Categories" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+
+    CONSTRAINT "Categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Questions" (
     "id" SERIAL NOT NULL,
     "categoryId" INTEGER NOT NULL,
@@ -11,6 +20,12 @@ CREATE TABLE "Questions" (
 
     CONSTRAINT "Questions_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Categories_title_key" ON "Categories"("title");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Categories_slug_key" ON "Categories"("slug");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Questions_question_key" ON "Questions"("question");
